59026a9d5d21f6d2811db85a629ea98e2038299b
Lequidex <82209443+Lequidex@users.noreply.github.com>
2025-12-05 23:57:19 +0300

    Fix formatting and clean up sezar.c

--- sezar.c
+++ sezar.c
@@ -11,19 +11,13 @@ int main(){
     scanf("%d", &anahtar);
 
     for(i=0; metin[i]!= '\0'; i++){
-        if(metin[i]>='A' && metin[i]<='Z'){
-            
+        if(metin[i]>='A' && metin[i]<='Z'){            
             metin[i]= ((metin[i] - 65 + anahtar) % 26) + 65;
 
-        } if(metin[i]>= 'a' && metin[i]<= 'z'){
-            
+        } if(metin[i]>= 'a' && metin[i]<= 'z'){           
             metin[i]= ((metin[i] -97+ anahtar) % 26) + 97;
         }
     }
     printf("Yeni metin: %s", metin);
-
-
-
-
     return 0;
 }
